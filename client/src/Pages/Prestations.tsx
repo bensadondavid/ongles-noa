b1dff10835207532ae2adb46aa30b106b6cd30bc
David <bensadondavidn@gmai.com>
2025-10-08 18:26:01 +0300

    changes

--- client/src/Pages/Prestations.tsx
+++ client/src/Pages/Prestations.tsx
@@ -1,10 +1,18 @@
+import { useSelector } from "react-redux"
 import { Link } from "react-router-dom"
 
 function Prestations() {
 
+  const languageState = useSelector(state => state.language)
+  const language = [
+    {french : 'Prestations', hebrew : 'טיפול'},
+    {french : 'liste', hebrew : "רשימה"}
+  ]
+
   return (
     <div className="prestations">
-
+      <p className="prestations-title">{languageState === 'french' ? language[0].french : languageState === "hebrew" ? language[0].hebrew : ""}</p>
+      <p className="prestations-subtitle">{languageState === 'french' ? language[1].french : languageState === "hebrew" ? language[1].hebrew : ""}</p>
     </div>
   )
 
